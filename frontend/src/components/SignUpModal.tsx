b6599ee5d54894ec7e57ee5a39f675d4b382e4f1
heonjwa <heonjwa@gmail.com>
2023-08-21 15:24:20 +0900

    copied from previous file

---
+++ frontend/src/components/SignUpModal.tsx
@@ -0,0 +1,15 @@
+import { User } from "../models/user";
+
+interface SignUpModalProps {
+    onDismiss: () => void,
+    onSignUpSuccessful: (user: User) => void,
+}
+
+const SignUpModal = ({onDismiss, onSignUpSuccessful}): SignUpModalProps => {
+
+    const { register, handleSubmit, formState: }
+    return (  );
+}
+
+
+export default SignUpModal;

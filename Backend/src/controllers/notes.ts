646732c229a3245858fd613352a625627711dd60
heonjwa <heonjwa@gmail.com>
2023-08-28 11:44:46 +0900

    user auth with middleware

--- Backend/src/controllers/notes.ts
+++ Backend/src/controllers/notes.ts
@@ -2,10 +2,15 @@ import { RequestHandler } from "express";
 import NoteModel from "../models/note";
 import createHttpError from "http-errors";
 import mongoose from "mongoose";
+import { assertIsDefined } from "../util/assortIsDefined";
 
 export const getNotes: RequestHandler = async(req, res, next) => {
+    const authenticatedUserId = req.session.userId;
+
     try {
-        const notes = await NoteModel.find().exec();
+        assertIsDefined(authenticatedUserId);
+
+        const notes = await NoteModel.find({userId: authenticatedUserId}).exec();
         res.status(200).json(notes);
     } catch (error) {
         next(error);
@@ -14,8 +19,11 @@ export const getNotes: RequestHandler = async(req, res, next) => {
 
 export const getNote: RequestHandler = async(req, res, next) => {
     const noteId = req.params.noteId;
+    const authenticatedUserId = req.session.userId;
     
     try {
+        assertIsDefined(authenticatedUserId);
+
         if(!mongoose.isValidObjectId(noteId)) {
             throw createHttpError(400, "Invalid note id");
         }
@@ -24,6 +32,10 @@ export const getNote: RequestHandler = async(req, res, next) => {
 
         if(!note) {
             throw createHttpError(404, "Note not found");
+        }
+
+        if(!note.userId.equals(authenticatedUserId)) {
+            throw createHttpError(401, "You cannot access this note");
         }
 
         res.status(200).json(note);
@@ -40,12 +52,16 @@ interface CreateNoteBody {
 export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
+    const authenticatedUserId = req.session.userId;
 
     try {
+        assertIsDefined(authenticatedUserId);
+
         if(!title) {
             throw createHttpError(400, "Note must have a title");
         }
         const newNote = await NoteModel.create({
+            userId: authenticatedUserId,
             title: title,
             text: text,
         });
@@ -68,8 +84,11 @@ export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBod
     const noteId = req.params.noteId;
     const newTitle = req.body.title;
     const newText = req.body.text;
+    const authenticatedUserId = req.session.userId;
 
     try {
+        assertIsDefined(authenticatedUserId);
+
         if(!mongoose.isValidObjectId(noteId)) {
             throw createHttpError(400, "Invalid note id");
         }
@@ -82,7 +101,11 @@ export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBod
 
         if(!note) {
             throw createHttpError(404, "Note not found");
-        }   
+        }
+
+        if(!note.userId.equals(authenticatedUserId)) {
+            throw createHttpError(401, "You cannot access this note");
+        }
 
         note.title = newTitle;
         note.text = newText;
@@ -97,20 +120,28 @@ export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBod
 
 export const deleteNote: RequestHandler = async(req, res, next) => {
     const noteId = req.params.noteId;
+    const authenticatedUserId = req.session.userId;
 
     try {
+        assertIsDefined(authenticatedUserId);
+
         if(!mongoose.isValidObjectId(noteId)) {
             throw createHttpError(400, "Invalid note id");
         }
 
-        const result = await NoteModel.deleteOne({ _id: noteId}).exec();
+        const note = await NoteModel.findById(noteId).exec();
 
-        if(result.deletedCount === 0) {
+        if(!note) {
             throw createHttpError(404, "Note not found");
         }
 
-        res.sendStatus(204);
+        if(!note.userId.equals(authenticatedUserId)) {
+            throw createHttpError(401, "You cannot access this note");
+        }
 
+        await note.deleteOne();
+
+        res.sendStatus(204);
     } catch (error) {
         next(error);
     }
